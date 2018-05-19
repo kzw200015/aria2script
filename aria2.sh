0627507bff4c4fa0c4234752065d8b775eb56e47
kzw200015 <kzw200015@gmail.com>
2018-05-19 23:36:53 +0800

    修改源码地址为realese

--- aria2.sh
+++ aria2.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+version="1.34.0"
+
 apt update && apt install -y gcc g++ libgnutls28-dev nettle-dev libssh2-1-dev libc-ares-dev libxml2-dev zlib1g-dev libsqlite3-dev pkg-config libcppunit-dev autoconf automake make autotools-dev autopoint libtool git &&
-git clone https://github.com/aria2/aria2.git &&
-cd aria2/src &&
+wget https://github.com/aria2/aria2/releases/download/release-${version}/aria2-${version}.tar.gz &&
+tar zxf aria2-${version}.tar.gz &&
+cd aria2-${version}/src &&
 rm -f OptionHandlerFactory.cc &&
 wget https://raw.githubusercontent.com/kzw200015/aria2script/master/OptionHandlerFactory.cc &&
 cd .. &&
-autoreconf -i &&
 ./configure &&
 make && make install &&
 echo "Installed successfully"
